a3a839b5ce779594f3818be7bcaa5de9ccb589a9
RandallFlagg <434681+RandallFlagg@users.noreply.github.com>
2020-11-27 01:07:52 +0200

    Update bot.js

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -5,7 +5,12 @@ const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
 bot.start(ctx => {
   return startAction(ctx)
-})
+});
+
+// bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 
 exports.handler = async event => {
   try {
